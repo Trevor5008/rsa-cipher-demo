0a4e3c9ce6976b11d3706c53d7b7bb41e21e7c9e
Trevor Werner <trezrevner@icloud.com>
2024-11-19 22:03:08 -0500

    Fixed config issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [publicKey, setPublicKey] = useState({ n: "" });
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   const handleInputChange = (event) => {
+      const { name, value } = event.target;
+      setPublicKey({ ...publicKey, [name]: value });
+   };
+
+   return (
+      <>
+         <h1>RSA Cipher Demo: {publicKey.n}</h1>
+         <div>
+            <input
+               type="text"
+               id="n"
+               name="n"
+               value={publicKey.n}
+               onChange={handleInputChange}
+               placeholder="Enter modulus (n)"
+               className="border border-gray-300 p-2 rounded w-full mb-4"
+            />
+         </div>
+         <div>
+            <input
+               type="text"
+               id="e"
+               name="e"
+               value={publicKey.e}
+               onChange={handleInputChange}
+               placeholder="Enter public exponent (e)"
+               className="border border-gray-300 p-2 rounded w-full mb-4"
+            />
+         </div>
+      </>
+   );
 }
 
-export default App
+export default App;
