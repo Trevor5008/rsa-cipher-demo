231e54f32cb9bff6fbbf6d337b4ce03ca0190937
Trevor Werner <trezrevner@icloud.com>
2024-11-20 21:04:20 -0500

    Fixed encrypt route, renders encrypted phrase to UI, replaced input boxes w/ textarea (needs styling)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,58 @@
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-   const [publicKey, setPublicKey] = useState({ n: "" });
+   const [publicKey, setPublicKey] = useState({ n: "", e: 65537 });
+   const [phrase, setPhrase] = useState("");
+   const [showPhrase, setShowPhrase] = useState(false);
+   const [encryptedPhrase, setEncryptedPhrase] = useState("");
+
+   useEffect(() => {
+      setPhrase("Hello World!")
+   }, [])
 
    const handleInputChange = (event) => {
       const { name, value } = event.target;
       setPublicKey({ ...publicKey, [name]: value });
    };
 
+   const handleEncrypt = () => {
+      // Encrypt the phrase using the public key
+      // and display the result
+      axios.post("http://127.0.0.1:5000/encrypt", 
+         { publicKey, phrase },
+         { headers: { "Content-Type": "application/json" } }
+         )
+         .then((response) => {
+            const phrase = response.data["encrypted_message"]
+            setEncryptedPhrase(phrase)
+         })
+         .catch((error) => {
+            console.error("Error encrypting the phrase:", error);
+         });
+   }
+
    return (
       <>
-         <h1>RSA Cipher Demo: {publicKey.n}</h1>
+         <h1>RSA Cipher Demo</h1>
+         <h2>Phrase: {showPhrase ? phrase : "***********"}</h2>
          <div>
-            <input
-               type="text"
-               id="n"
-               name="n"
-               value={publicKey.n}
+            <textarea 
+               name="n" 
+               id="mod" 
                onChange={handleInputChange}
-               placeholder="Enter modulus (n)"
                className="border border-gray-300 p-2 rounded w-full mb-4"
+               value={publicKey.n || ""} 
+               placeholder="Enter modulus (n)"
             />
+            {showPhrase && 
+               <div
+                  className="text-sm text-gray-500 z-20" 
+                  hidden={!showPhrase}>
+                  Phrase: {phrase}
+               </div>}
          </div>
+         <button className="m-2 bg-red-500 rounded-lg p-2" onClick={() => setShowPhrase(!showPhrase)}>Show Phrase</button>
          <div>
             <input
                type="text"
@@ -33,6 +64,8 @@ function App() {
                className="border border-gray-300 p-2 rounded w-full mb-4"
             />
          </div>
+         <button className="m-2 bg-blue-500 rounded-lg p-2" onClick={() => handleEncrypt()}>Encrypt</button>
+         <textarea name="" id="" className="border border-gray-300 p-2 rounded w-full mb-4" readOnly value={encryptedPhrase}/>
       </>
    );
 }
